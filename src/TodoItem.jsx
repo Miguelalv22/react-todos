db1af56704be1a88dd4ea6349a34e2599dd6d130
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-05-29 13:59:02 -0600

    Se crea un componente form para nuevas tareas y se le da funcionamiento

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
