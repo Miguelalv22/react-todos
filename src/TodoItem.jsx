5e5f405beb78b21f7294100df1e362fce51b2e4b
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-05-30 11:15:13 -0600

    Se agrega un icono adecuado para elliminar los todos y se cambia el mensaje del Form

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -5,7 +5,7 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function TodoItem({ todo, removeTodo, toggleTodo }) {
     const labelId = `checkbox-list-label-${todo.id}`;
@@ -17,7 +17,7 @@ export default function TodoItem({ todo, removeTodo, toggleTodo }) {
         <ListItem
             secondaryAction={
                 < IconButton edge="end" aria-label="comments" onClick={removeTodo} >
-                    <CommentIcon />
+                    <DeleteIcon />
                 </IconButton >
             }
             disablePadding
